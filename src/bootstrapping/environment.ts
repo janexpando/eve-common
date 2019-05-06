10813f690b32f3c5edbbe2bd7e488af1ec2f4d97
Jan Kremen <jan@expan.do>
2019-05-06 18:04:25 +0200

    better env variables checking

--- src/bootstrapping/environment.ts
+++ src/bootstrapping/environment.ts
@@ -1,23 +1,37 @@
 import 'dotenv/config';
-import {Imparsable, imparsable, property} from 'imparsable';
 import {Provider, Type} from "injection-js";
+import {object, number, string} from 'joi';
 
 export function makeCreateEnvironment<T extends Environment>(type: Type<T>) {
     return (override?: Partial<T>): T => {
-        let env = Imparsable.parsePojo(type, process.env);
-        if (override) return Object.assign(env, override);
+        let env = new type();
+        env.init();
         return env;
     }
 }
 
-@imparsable()
 export class Environment {
-    @property() PORT: number = 8080;
-    @property() SENTRY_DSN: string;
-    @property() NODE_ENV: string;
-    @property() MONGODB_URI: string = 'mongodb://127.0.0.1:27017/app';
-    @property() EVE_AUTH_BEARER: string;
-    @property() SERVICE_URL: string;
+    PORT: number = 8080;
+    SENTRY_DSN: string;
+    NODE_ENV: string;
+    MONGODB_URI: string;
+    EVE_AUTH_BEARER: string;
+    SERVICE_URL: string;
+
+
+
+    schema = object({
+        PORT: number().default(3333),
+        SENTRY_DSN: string(),
+        NODE_ENV: string().required(),
+        MONGODB_URI: string().default('mongodb://127.0.0.1:27017/app'),
+        EVE_AUTH_BEARER: string().required(),
+        SERVICE_URL: string().required()
+    });
+
+    init(){
+        this.schema
+    }
 
     static create = makeCreateEnvironment(Environment);
 }
@@ -26,3 +40,7 @@ export const ENVIRONMENT_PROVIDER: Provider = {
     provide: Environment,
     useValue: Environment.create()
 };
+
+class Env2 extends Environment{
+
+}
