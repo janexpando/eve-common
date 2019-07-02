c251422869bbc3d881fdd22bf3374bf4f0e83af6
Vit <wictimcz@gmail.com>
2019-07-02 14:22:36 +0200

    adding 2 clients

---
+++ src/clients/mongo-logger-client.ts
@@ -0,0 +1,18 @@
+import { Injectable } from 'injection-js';
+import { ObjectId } from 'bson';
+import {Environment} from "../bootstrapping/environment";
+import {EveClient} from "./eve-client";
+
+@Injectable()
+export class MongoLoggerClient extends EveClient {
+    constructor(protected env: Environment) {
+        super(env);
+        this.baseUrl = this.env.PRODUCT_SERVICE_URL;
+    }
+
+    async getLastSynced(companyId: ObjectId): Promise<Date> {
+        const url = `/company/${companyId}/log/last-synced`;
+        let response = await this.got.get(url);
+        return response.body && response.body.lastSynced;
+    }
+}
---
+++ src/clients/product-feed-result-client.ts
@@ -0,0 +1,37 @@
+import { Injectable } from 'injection-js';
+import { ObjectId } from 'bson';
+import {Environment} from "../bootstrapping/environment";
+import {EveClient} from "./eve-client";
+
+export type ProductFeedType =
+    | "GOOGLE_PRODUCT"
+    | "SHOPIFY"
+    | "SHOPTET"
+    | "NONE"
+    | "CUSTOM";
+
+export interface ApiProductFeedResult {
+    companyId: ObjectId;
+    productsInStock: number;
+    productsWithEAN: number;
+    productsAll: number;
+    productsToBeListed: number;
+    feedUrl: string;
+    feedType: ProductFeedType;
+    currency: string;
+    error: string | null;
+}
+
+@Injectable()
+export class ProductFeedResultClient extends EveClient {
+    constructor(protected env: Environment) {
+        super(env);
+        this.baseUrl = this.env.PRODUCT_SERVICE_URL;
+    }
+
+    async get(companyId: ObjectId): Promise<ApiProductFeedResult> {
+        const url = `/company/${companyId}/product-feed-result`;
+        let response = await this.got.get(url);
+        return response.body;
+    }
+}
