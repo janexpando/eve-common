b71cdd54b12a685bfe4c9ada3ee559f8f8045e3a
Jan Kremen <jan@expan.do>
2019-07-29 13:24:35 +0200

    tempfile should be able to use /tmp/ directory because of the google cloud functions

--- src/stream/temp-file.ts
+++ src/stream/temp-file.ts
@@ -1,11 +1,21 @@
 import * as fs from 'fs';
 import { promisify } from 'util';
 
-export class TempFile {
-    tempDir = fs.mkdtempSync('product-feed');
-    file = `${this.tempDir}/tempfile.${this.extention}`;
+export interface TempFileOptions {
+    extention?: string;
+    prefix?: string;
+}
 
-    constructor(public extention: string = 'temp') {}
+export class TempFile {
+    tempDir:string;
+    file: string;
+
+    constructor(public options?: TempFileOptions) {
+        if(!options) options = {};
+        let prefix = options.prefix || 'product-feed';
+        this.tempDir = fs.mkdtempSync(prefix);
+        this.file = `${this.tempDir}/tempfile`
+    }
 
     createWriteStream() {
         return fs.createWriteStream(this.file);
@@ -13,6 +23,7 @@ export class TempFile {
     createReadStream() {
         return fs.createReadStream(this.file);
     }
+
     async dispose() {
         await promisify(fs.unlink)(this.file);
         await promisify(fs.rmdir)(this.tempDir);
