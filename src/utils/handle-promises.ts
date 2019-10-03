1228e25c8b11986f8725cbc8799eeb8e0bfc840e
Vit <wictimcz@gmail.com>
2019-10-03 11:09:55 +0200

    adding pipelinify and handlePromises functions

---
+++ src/utils/handle-promises.ts
@@ -0,0 +1,10 @@
+export async function handlePromises<T>(promises: Promise<T>[]): Promise<{ result: T, error: Error }[]> { // TODO tests
+    let packedPromises = promises.map(p => {
+        return p.then(r => {
+            return {result: r, error: null}
+        }).catch(e => {
+            return {result: null, error: e}
+        })
+    });
+    return await Promise.all(packedPromises);
+}
