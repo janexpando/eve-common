808ded120c80dba0c6ebd9c431db815603cf9b4e
Vit <wictimcz@gmail.com>
2019-10-09 16:10:28 +0200

    adding CZK currency for mall

--- src/models/currency.ts
+++ src/models/currency.ts
@@ -1,2 +1,2 @@
-export type CurrencyCode = 'EUR' | 'GBP' | 'USD' | 'CAD';
-export const CURRENCY_CODES = ['EUR', 'GBP', 'USD', 'CAD'];
+export type CurrencyCode = 'EUR' | 'GBP' | 'USD' | 'CAD' | 'CZK';
+export const CURRENCY_CODES = ['EUR', 'GBP', 'USD', 'CAD', 'CZK'];
