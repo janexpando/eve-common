078ed18ca59a8fc16585f36d1308fd428ced3c37
Vit <wictimcz@gmail.com>
2019-10-10 15:40:08 +0200

    adding remaining currencies to currencyCode type

--- src/models/currency.ts
+++ src/models/currency.ts
@@ -1,2 +1,2 @@
-export type CurrencyCode = 'EUR' | 'GBP' | 'USD' | 'CAD' | 'CZK';
-export const CURRENCY_CODES = ['EUR', 'GBP', 'USD', 'CAD', 'CZK'];
+export type CurrencyCode = 'EUR' | 'GBP' | 'USD' | 'CAD' | 'CZK' | 'MXN' | 'BRL' | 'INR' | 'CNY' | 'JPY' | 'AUD';
+export const CURRENCY_CODES = ['EUR', 'GBP', 'USD', 'CAD', 'CZK', 'MXN', 'BRL', 'INR', 'CNY', 'JPY', 'AUD'];
