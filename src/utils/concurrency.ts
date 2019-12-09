a1e3f9ea4da097157f8fcc428594833f38ad9b0b
Vit <wictimcz@gmail.com>
2019-12-09 15:03:34 +0100

    adding function for running jobs concurrently

---
+++ src/utils/concurrency.ts
@@ -0,0 +1,30 @@
+import {sleep} from "../utils";
+
+export async function runConcurrentJobs(
+    concurrency: number,
+    job: () => Promise<void>,
+    limit: number = 0,
+    exceptionCatch: (e) => void = (e) => { },
+    updateConcurrencyJob: () => Promise<number> = null,
+    updateConcurrencyPeriod_ms: number = 600000,
+    tickTime_ms: number = 1000
+) {
+    if (tickTime_ms < 1)
+        throw 'tickTime_ms must be an integer greater than 0';
+    let counter = 0;
+    let nextConcurrencyUpdateAt = new Date().getTime() + updateConcurrencyPeriod_ms;
+    let infinite = (limit == 0);
+    while (infinite || (limit-- > 0)) {
+        if (counter < concurrency) {
+            counter++;
+            job()
+                .catch(exceptionCatch)
+                .finally(() => counter--);
+        }
+        await sleep(tickTime_ms);
+        if (updateConcurrencyJob != null && new Date().getTime() > nextConcurrencyUpdateAt) {
+            nextConcurrencyUpdateAt += updateConcurrencyPeriod_ms;
+            concurrency = await updateConcurrencyJob();
+        }
+    }
+}
