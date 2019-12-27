c331e76ddd46d46d4628b9442c1798f434ceaa5f
Jan Kremen <kremenh@gmail.com>
2019-12-27 16:16:13 +0100

    adding method to get delivery methods

--- src/clients/mall-service-client.ts
+++ src/clients/mall-service-client.ts
@@ -1,11 +1,28 @@
-import {EveClient} from "./eve-client";
-import {Environment} from "../bootstrapping/environment";
-import {Injectable} from "injection-js";
+import { EveClient } from "./eve-client";
+import { Environment } from "../bootstrapping/environment";
+import { Injectable } from "injection-js";
+import { ObjectId } from "bson";
+import { MallType } from "../models/marketplace-names";
 
 @Injectable()
 export class MallServiceClient extends EveClient {
-    constructor(protected env: Environment) {
-        super(env);
-        this.baseUrl = this.env.MALL_SERVICE_URL;
-    }
+  constructor(protected env: Environment) {
+    super(env);
+    this.baseUrl = this.env.MALL_SERVICE_URL;
+  }
+
+  async getDeliveryMethods(
+    companyId: ObjectId
+  ): Promise<ApiMallDeliveryMethod[]> {
+    let response = await this.got.get(
+      `/company/${companyId}/mall-delivery-methods`
+    );
+    return response.body;
+  }
+}
+
+export interface ApiMallDeliveryMethod {
+  code: string;
+  title: string;
+  marketplace: MallType;
 }
