0d184481f1a6592b8506cd095899c0e45936fb43
Vit <wictimcz@gmail.com>
2020-04-20 14:41:28 +0200

    changing company error client

--- src/clients/company-error-client.ts
+++ src/clients/company-error-client.ts
@@ -10,10 +10,10 @@ export class CompanyErrorClient extends EveClient {
         this.baseUrl = this.env.COMPANY_SERVICE_URL;
     }
 
-    async set(companyId: ObjectId, code: string, message?: string) {
+    async set(companyId: ObjectId, code: string, messages: string[] = []) {
         let response = await this.got.post(`/company/${companyId}/company-error`, {
             json: true,
-            body: { code, message },
+            body: { code, messages },
         });
     }
 }
