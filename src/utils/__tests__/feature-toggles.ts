c3ca02ec2493850768303fb550eafa761e3ba5a8
Radek Kysely <radek@kysely.org>
2020-08-04 16:15:33 +0200

    (change) Utility for resolving feature toggle

--- src/utils/__tests__/feature-toggles.ts
+++ src/utils/__tests__/feature-toggles.ts
@@ -1,47 +1,29 @@
 import { test } from '../../testing';
-import { shouldPass, shouldBlock } from '../feature-toggles';
+import { isFeatureEnabledFor } from '../feature-toggles';
 import { ApiFeatureToggle } from '../../clients/feature-toggle-client';
 
-test('`shouldPass` for "block" feature toggle gate', t => {
+test('`isFeatureOnFor` default to `false` if no toggle is provided', t => {
+    t.is(isFeatureEnabledFor('member-3', null), false);
+});
+
+test('`isFeatureOnFor` for "block" feature toggle gate', t => {
     const mockToggle: ApiFeatureToggle = {
         group: ['member-1', 'member-3'],
         name: 'auto-fulfillment',
         gate: 'block',
     };
 
-    t.is(shouldPass('member-3', mockToggle), false);
-    t.is(shouldPass('member-4', mockToggle), true);
+    t.is(isFeatureEnabledFor('member-3', mockToggle), false);
+    t.is(isFeatureEnabledFor('member-4', mockToggle), true);
 });
 
-test('`shouldPass` for "pass" feature toggle gate', t => {
+test('`isFeatureOnFor` for "pass" feature toggle gate', t => {
     const mockToggle: ApiFeatureToggle = {
         group: ['member-1', 'member-3'],
         name: 'auto-fulfillment',
         gate: 'pass',
     };
 
-    t.is(shouldPass('member-3', mockToggle), true);
-    t.is(shouldPass('member-4', mockToggle), false);
-});
-
-test('`shouldBlock` for "block" feature toggle gate', t => {
-    const mockToggle: ApiFeatureToggle = {
-        group: ['member-1', 'member-3'],
-        name: 'auto-fulfillment',
-        gate: 'block',
-    };
-
-    t.is(shouldBlock('member-3', mockToggle), true);
-    t.is(shouldBlock('member-4', mockToggle), false);
-});
-
-test('`shouldBlock` for "pass" feature toggle gate', t => {
-    const mockToggle: ApiFeatureToggle = {
-        group: ['member-1', 'member-3'],
-        name: 'auto-fulfillment',
-        gate: 'pass',
-    };
-
-    t.is(shouldBlock('member-3', mockToggle), false);
-    t.is(shouldBlock('member-4', mockToggle), true);
+    t.is(isFeatureEnabledFor('member-3', mockToggle), true);
+    t.is(isFeatureEnabledFor('member-4', mockToggle), false);
 });
--- src/utils/feature-toggles.ts
+++ src/utils/feature-toggles.ts
@@ -1,11 +1,8 @@
-import { ApiFeatureToggle, FeatureToggleGate } from '../clients/feature-toggle-client';
+import { ApiFeatureToggle } from '../clients/feature-toggle-client';
 
-function should(gateType: FeatureToggleGate) {
-    return (member: string, featureToggle: ApiFeatureToggle): boolean => {
-        const isMemberOfGroup: boolean = featureToggle.group.includes(member);
-        return featureToggle.gate === gateType ? isMemberOfGroup : !isMemberOfGroup;
-    };
+export function isFeatureEnabledFor(member: string, featureToggle: ApiFeatureToggle | null): boolean {
+    if (!featureToggle) return false;
+
+    const isMemberOfGroup: boolean = featureToggle.group.includes(member);
+    return featureToggle.gate === 'pass' ? isMemberOfGroup : !isMemberOfGroup;
 }
-
-export const shouldPass = should('pass');
-export const shouldBlock = should('block');
